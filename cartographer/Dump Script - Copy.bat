47e6de6a35e51acd28687290a11e1832829a5467
Normmatt <normmatt234@gmail.com>
2011-05-08 17:17:10 +1200

    preliminary script dump.

---
+++ cartographer/Dump Script - Copy.bat
@@ -0,0 +1,4 @@
+cartographer ../rom/input.gba dinodevice2_commands_pointers.txt dinodevice2_script_pointers -s
+cartographer ../rom/input.gba dinodevice2_commands_pointers.txt dinodevice2_script_pointers -m
+
+pause
---
+++ cartographer/Dump Script.bat
@@ -0,0 +1,4 @@
+cartographer ../rom/input.gba dinodevice2_commands.txt dinodevice2_script -s
+cartographer ../rom/input.gba dinodevice2_commands.txt dinodevice2_script -m
+
+pause
